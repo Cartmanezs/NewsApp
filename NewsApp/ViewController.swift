59fec0621c3669b7b0387c2a7a99ec697d297c81
Igor Palyvoda <tib.igor.mk@gmail.com>
2021-04-15 10:40:41 +0300

    first

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Ingvar on 15.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
